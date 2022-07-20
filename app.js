2cd4c2630b38238ab54af39ea72524ce51a3c780
Jacob Wachtel <jacob.wachtel@transactcampus.com>
2022-07-20 09:37:56 -0400

    Updated: Connection to db cluster

--- app.js
+++ app.js
@@ -8,49 +8,22 @@ const ObjectId = require('mongodb').ObjectId;
 const Tool = require('./models/Schema')
 
 
-// app.use(express.static('./public'));
 app.use(express.json());
 
-
-// const errorCatcher = (inputError) => {
-//     console.log(inputError);
-//     res.status(500);
-//     res.json({ status: 500, error: inputError })
-// }
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     next();
-//  });
-
 app.get('/api/v1/tools', (req, res) => {
-//    let db_connect = connectDB.getDb('evans-db');
-//    db_connect
-//    .collection('tools')
-//    .find({})
-//    .toArray((err, result) => {
-//     if(err) throw err;
-//     res.json(result);
-//    })
-    Tool.find({}, (err, found) => {
-        if (!err) {
 
-            res.send(found);
+    Tool.find({}, (err, found) => {
+        if (err) {
+            console.log("ERROR!!!")
+            return res.send("no data found");
         }
-        console.log(err);
-        res.send("Some error occurred!");
+        if (found) {
+            res.status(200).send(found);
+        }
+        
     })
 })
 
 app.listen(3000, () => {
     console.log('listening on Port 3000');
 })
-
-
-// const tasks = Task.find({});
-// collection.find()
-//     .toArray()
-//     .then((docs) => res.json(docs))
-//     .catch((err) => errorCatcher(err))
-
-// res.status(200).json({ message: 'GET request to homepage'});
